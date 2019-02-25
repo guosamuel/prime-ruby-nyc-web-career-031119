b38058eb0b5a719773b85b13c91af2c0d8f7ca1e
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 23:32:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,6 +5,8 @@ def prime?(integer)
     return false
   elsif integer == 1
     return false
+  elsif integer == 0
+    return false
   end 
   
   [*2..integer-1].none? { |divisor| integer % divisor == 0}
