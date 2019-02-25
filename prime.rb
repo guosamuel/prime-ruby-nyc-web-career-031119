711b7f82f8c99fb1fc48c0b50e42622d137dc0f9
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 23:02:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,4 +7,12 @@ def prime?(integer)
   end 
   
   prime_array = [2, 3, 5, 7, 11]
+  counter = 2
+  
+  until counter == 999999
+    prime_array.each do |prime_number|
+      if counter % prime_number != 0
+        prime_array.push
+        counter += 1
+    end
 end
