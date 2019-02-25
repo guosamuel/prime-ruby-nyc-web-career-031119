5eb8f41d233ebe67e2d71dcbdd3c23cdc956d95a
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 23:26:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,22 +7,5 @@ def prime?(integer)
     return false
   end 
   
-  prime_array = [2]
-  counter = 3
-  
-  until counter == 999999
-    prime_array.each do |prime_number|
-      if counter % prime_number > 0
-        prime_array.push(counter)
-        counter += 1
-      end
-    end
-  end
-  
-  prime_aray.each do |prime_number2|
-    if integer == prime_number2
-      return true
-    end
-  end
   
 end
