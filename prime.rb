eed99cbb14dda11ad21471482f9ca8c1778ba718
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 22:25:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(integer)
-  if integer.abs == 2
+  if integer < 0
+    return false
+  elsif integer.abs == 2
     return true
   elsif integer.abs % 2 == 0
     return false
@@ -10,7 +12,5 @@ def prime?(integer)
     return false
   elsif integer.abs % 6 == 1 || integer.abs % 6 == 5
     return true
-  elsif integer < 0
-    return false
   end
 end
