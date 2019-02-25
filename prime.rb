6391e19aabc23174d3552942d88a5429784e04c9
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 23:30:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 # Add  code here!
 def prime?(integer)
-  n = integer
+  
   if integer < 0
     return false
   elsif integer == 1
     return false
   end 
   
+  testing_array = Array.new()
   [2..n-1].none? { |divisor| integer % divisor == 0}
   
 end
